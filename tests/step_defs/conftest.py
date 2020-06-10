199cecdb67bc061deadb46a4e5461b9ea8d8886d
vjanz <valon.januzaj98@gmail.com>
2020-06-10 21:49:17 +0200

    Updated commons in conftest.py

--- tests/step_defs/conftest.py
+++ tests/step_defs/conftest.py
@@ -1,9 +1,11 @@
 import os
 
+import requests
 from pytest_bdd import given
 
 BDD_BASE_URL = os.getenv("BDD_BASE_URL")
-
+SUPERUSER_EMAIL = os.getenv("SUPERUSER_EMAIL")
+SUPERUSER_PASSWORD = os.getenv("SUPERUSER_PASSWORD")
 
 @given("API endpoint for user registration")
 def api_endpoint() -> str:
@@ -15,3 +17,29 @@ def api_endpoint() -> str:
 def login_api_url():
     LOGIN_API_ENDPOINT = f"{BDD_BASE_URL}/login/"
     return LOGIN_API_ENDPOINT
+
+
+@given("API endpoint for categories")
+def categories_endpoint():
+    CATEGORIES_ENDPOINT = f"{BDD_BASE_URL}/categories"
+    return CATEGORIES_ENDPOINT
+
+
+
+@given("User is logged in")
+def user_logged_in():
+    data = {
+        "email": SUPERUSER_EMAIL,
+        "password": SUPERUSER_PASSWORD
+    }
+
+    r = requests.post(
+        f"{BDD_BASE_URL}/login/", data=data
+    )
+    tokens = r.json()
+    access_token = tokens["access_token"]
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    return headers
+
